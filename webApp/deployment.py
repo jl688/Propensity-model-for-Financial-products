804d3077997d269f82e565560f3d079bf37f6c7c
kanha gupta <92207457+kanha-gupta@users.noreply.github.com>
2023-11-08 08:07:34 +0000

    refactor

--- webApp/deployment.py
+++ webApp/deployment.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import streamlit as st
 from PIL import Image
+import os
 
 #this shows the output of this model & how this can be useful.
 st.set_page_config(page_title='Model output')
 st.header('Model output for both products')
 
 
-predicted_dataset = r'Data/df_pred.xlsx'
-recommended_dataset = r'Data/df_final_recommend.xlsx'
+predicted_dataset = r'webApp/data/df_pred.xlsx'
+recommended_dataset = r'webApp/data/df_final_recommend.xlsx'
 
 st.subheader('See Predicted values')
 df_predicted = pd.read_excel(predicted_dataset)
